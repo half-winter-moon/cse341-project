d2bd0989b258590a9ab8e1c55885da3aa1db696e
half-winter-moon <han16064@byui.edu>
2022-06-01 17:14:45 -0600

    added instance of node packages that should be helpful for authentication. set config variable to an object that refers to the variebles set in the .env file

--- server.js
+++ server.js
@@ -7,11 +7,27 @@ const mongodb = require('./db/connect');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
+// authentication stuff
+const { auth, requiresAuth } = require('express-openid-connect');
+require('dotenv').config();
+
 const port = process.env.PORT || 8080;
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+};
 
 var options = {
   explorer: true
 };
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
 
 app
  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options))
